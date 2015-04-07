3549d28b640efdbb5854c323f2846db036ff80de
Dustin Myers <dustint314@gmail.com>
2015-04-07 14:44:31 -0600

    users are created

--- instanceArray.js
+++ instanceArray.js
@@ -13,12 +13,14 @@ var User = function(name, email, pw){
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
-
+var users = [];
 
 //Now create and push into your users array 3 seperate instances of User using the data from above in that exact order
 
-  //code here
+users.push(user('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS'));
+users.push(user('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags'));
+users.push(user('Tyler', 'lenny@theLenster.com', 'iLoveLentilSoup'));
+
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
